3d9aaefe6095d3d31c86f03140b1be7add3abe44
Stefan Vercillo <svercillo7@gmail.com>
2025-11-29 21:11:44 -0500

    Update wiggle-sort.py

--- medium/wiggle-sort.py
+++ medium/wiggle-sort.py
@@ -1,1 +1,16 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums) 
+        gte = True
 
+        for i in range(n -1):
+            if (nums[i +1] >= nums[i]) != gte:
+                t = nums[i]
+                nums[i] = nums[i+1]
+                nums[i+1] = t
+            gte = not gte
+
+        
